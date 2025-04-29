625c20e644110486266e7514bd8d1214a60d09da
Luca Tsun-Yien Cheung <lacuna64469@gmail.com>
2025-04-29 15:40:58 +0100

    Add files via upload

---
+++ code.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+# Sample Python code for youtube.playlists.list
+# See instructions for running these code samples locally:
+# https://developers.google.com/explorer-help/code-samples#python
+
+import os
+
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def main():
+    # Disable OAuthlib's HTTPS verification when running locally.
+    # *DO NOT* leave this option enabled in production.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = r"C:\Users\Ayisha\AppData\Local\Programs\Python\Python313\bob\client_secret_987114519253-48p3h20ilfjq1uaqfled9abnp0vgdgis.apps.googleusercontent.com.json"
+
+    # Get credentials and create an API client
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
+        client_secrets_file, scopes)
+    credentials = flow.run_local_server(port=8080)
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, credentials=credentials)
+
+    request = youtube.playlistItems().list(
+        part="snippet",
+        fields="nextPageToken,items/snippet/title,items/snippet/position,items/snippet/videoOwnerChannelTitle,items/snippet/publishedAt",
+        maxResults=50,
+        playlistId="PL0Z5QbVE4dX2QXuSoK-MNhmXcswwHeC1S"
+    )
+    response = request.execute()
+
+    print(response)
+
+if __name__ == "__main__":
+    main()
